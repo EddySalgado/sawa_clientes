ed7ec88facf17b1f8581d9c87b27c1eab1ee1bde
MiguelAngel <aldkl@jdjd.com>
2024-10-12 10:43:15 -0600

    fixed hhtpclient injection error

---
+++ src/app/core/service/AuthService.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+interface LoginResponse {
+  login: boolean;
+  status: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'https://sawapi.up.railway.app/api';
+
+  constructor(private http: HttpClient) { }
+
+  login(correo: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, { correo, password });
+  }
+}
